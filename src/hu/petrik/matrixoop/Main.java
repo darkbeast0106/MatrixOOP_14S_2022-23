427db8302db0b158aba2b9193379d578a6cb7248
Budaházi Bence <budahazi.bence@petrik.hu>
2022-10-20 16:20:55 +0200

    Feladatok elso pozitív elemig

--- src/hu/petrik/matrixoop/Main.java
+++ src/hu/petrik/matrixoop/Main.java
@@ -8,5 +8,16 @@ public class Main {
         System.out.println(m1);
         System.out.println();
         System.out.println(m2);
+        System.out.printf("A mátrix elemeinek összege: %d, pozitív elemeinek száma: %d, legnagyobb eleme: %d\n",
+                m2.getElemekOsszege(), m2.getPozitivElemekSzama(), m2.getLegnagyobbElem());
+        int[] elsoPozitiv = m2.findElsoPozitivElem();
+        System.out.printf("A mátrix első pozitív eleme %d,%d\n", elsoPozitiv[0], elsoPozitiv[1]);
+
+        for (int i = 0; i < 100; i++) {
+            Matrix m = new Matrix(2,2);
+            int[] elso = m.findElsoPozitivElem();
+            System.out.println(m);
+            System.out.printf("A mátrix első pozitív eleme %d,%d\n", elso[0], elso[1]);
+        }
     }
 }
--- src/hu/petrik/matrixoop/Matrix.java
+++ src/hu/petrik/matrixoop/Matrix.java
@@ -19,6 +19,60 @@ public class Matrix {
         }
     }
 
+    public int getElemekOsszege() {
+        int osszeg = 0;
+        for (int[] sor : this.m) {
+            for (int elem : sor) {
+                osszeg += elem;
+            }
+        }
+        return osszeg;
+    }
+
+    public int getPozitivElemekSzama() {
+        int db = 0;
+        for (int[] sor : this.m) {
+            for (int elem : sor) {
+                if (elem > 0) {
+                    db++;
+                }
+            }
+        }
+        return db;
+    }
+
+    public int getLegnagyobbElem() {
+        int legnagyobb = this.m[0][0];
+        //oszlopfolytonos bejárás
+        for (int i = 0; i < this.m[0].length; i++) {
+            for (int j = 0; j < this.m.length; j++) {
+                if (legnagyobb < this.m[j][i]) {
+                    legnagyobb = this.m[j][i];
+                }
+            }
+        }
+        return legnagyobb;
+    }
+
+    public int[] findElsoPozitivElem() {
+        int sorIndex = 0;
+        int oszlopIndex = 0;
+        while (sorIndex < this.m.length && this.m[sorIndex][oszlopIndex] < 1) {
+            while (oszlopIndex < this.m[sorIndex].length && this.m[sorIndex][oszlopIndex] < 1) {
+                oszlopIndex++;
+            }
+            if (oszlopIndex == this.m[sorIndex].length) {
+                oszlopIndex = 0;
+                sorIndex++;
+            }
+        }
+        if (sorIndex == this.m.length) {
+            sorIndex = -1;
+            oszlopIndex = -1;
+        }
+        return new int[] {sorIndex, oszlopIndex};
+    }
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
